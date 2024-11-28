4b344d9e9b56f2070b79e4e56291e367ebf035dc
ShaneKavinda <shanekavinda1999@gmail.com>
2024-11-28 19:25:21 +0530

    Initial Commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div
+        class="flex min-h-screen flex-col items-center bg-cover bg-center pt-6 sm:justify-center sm:pt-0"
+        :style="{ backgroundImage: `url('images/futuristic-robotic-hummingbird.jpg')` }"
+    >
+        <div>
+            <Link href="/">
+                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+            </Link>
+        </div>
+
+        <div
+            class="mt-6 w-full overflow-hidden px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
+            style="background-color: rgba(0,0,0,0.4)"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
